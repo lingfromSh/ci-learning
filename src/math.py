bcf8a53892cb7e61f3d88ec044e4b8a729852082
StephenLing <shiyun.ling@flexiv.com>
2022-01-21 15:31:14 +0800

    Add pylint in CI.

---
+++ src/math.py
@@ -0,0 +1,15 @@
+"""
+Math utils
+"""
+
+
+def plus(a: int, b: int) -> int:
+    """
+    Plus a with b.
+    :param a: int
+    :param b: int
+    :return: a + b
+    """
+    assert isinstance(a, int), "a must be type of int."
+    assert isinstance(b, int), "b must be type of int."
+    return a + b
